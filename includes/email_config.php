2e04e8364e9918b5b8dbb4219848cb80be0197a8
AaGho-Prodigy <budhathokip430@gmail.com>
2026-05-15 21:00:16 +0545

    Remove email credentials from tracking

--- includes/email_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('SMTP_HOST', 'smtp.gmail.com');
-define('SMTP_PORT', 587);
-define('SMTP_USERNAME', 'fivepratik@gmail.com'); 
-define('SMTP_PASSWORD', 'ugiy ccrk eges xrdp'); 
-define('FROM_EMAIL', 'fivepratik@gmail.com');
-define('FROM_NAME', 'Voting System');
-
